8ad465605214c7e386483e071c21d80ddbabaa20
victoramattosc <victorcarbelotti0306@gmail.com>
2023-08-10 08:53:32 -0300

    Img

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView } from 'react-native';
+import Carrinho from './src/pages/Carrinho/index';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Etec Cidade do Livro</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <StatusBar />
+      <Carrinho/>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,8 +1,15 @@
 import React from "react"
-import { Text } from "react-native";
+import { Image, StyleSheet } from "react-native";
+import topo from "../../../assets/topo.png"
 
 export default function Carrinho(){
     return(
-        <Text>Carrinho de Compras</Text>
+        <Image source={topo} style={styles.topo}/>
     );
 }
+
+const styles = StyleSheet.create({
+  topo: {
+    width: "100%"
+  }
+})
