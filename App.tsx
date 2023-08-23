46c1919fb5376071939280146b8bfdd6955fb593
victoramattosc <victorcarbelotti0306@gmail.com>
2023-08-23 15:47:16 -0300

    fonte e index

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
-import { StatusBar, SafeAreaView } from 'react-native';
-import Carrinho from './src/pages/Carrinho/index';
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_400Regular_Italic,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
+import { StatusBar, SafeAreaView } from "react-native";
+import Carrinho from "./src/pages/Carrinho/index";
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    "MRegular" : Montserrat_400Regular,
+    "MItalic" : Montserrat_400Regular_Italic,
+    "MBold" : Montserrat_700Bold
+  })
+
+  if(!fonteCarregada){
+    return null;
+  }
   return (
     <SafeAreaView>
       <StatusBar />
-      <Carrinho/>
+      <Carrinho />
     </SafeAreaView>
   );
 }
--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
 import topo from "../../../assets/topo.png";
+import logo from "../../../assets/logo.png"
 
 const width = Dimensions.get('screen').width;
 
@@ -9,9 +10,12 @@ export default function Carrinho(){
       <>
         <Image source={topo} style={styles.topo}/>
         <Text style={styles.titulo}>Detalhes do Carrinho</Text>
-        <View>
-          <Text>Carrinho de Compras</Text>
+        <View  style={styles.carrinho}>
+          <Text style={styles.nome}>Carrinho de Compras</Text>
+          <View style={styles.fazenda}>
+            <Image source={logo}/>
           <Text>Fazenda Etec Cidade do Livro</Text>
+          </View>
           <Text>Carrinho de Compras</Text>
           <Text>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para a sua cozinha</Text>
           <Text>R$40,00</Text>
@@ -33,7 +37,21 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 24,
     lineHeight: 26,
-    fontWeight: 'bold',
-    padding: 16
+    padding: 16,
+    fontFamily: 'MBold'
   },
+  carrinho: {
+    paddingVertical: 8,
+    paddingHorizontal: 16
+  },
+  nome: {
+    fontSize: 26,
+    lineHeight: 42,
+    color: '#4646464',
+    fontFamily: 'MBold'
+  },
+  fazenda: {
+    flexDirection: 'row',
+    paddingVertical: 12
+  }
 });
